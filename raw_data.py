2628d8c890817a1db73e6de66469cdeb5bc2d61e
ArrestedLightning <10822331+ArrestedLightning@users.noreply.github.com>
2023-04-06 22:32:23 -0500

    Use a queue to transfer scope data to main thread

--- raw_data.py
+++ raw_data.py
@@ -4,14 +4,13 @@ from datetime import datetime
 import matplotlib
 import matplotlib.pyplot as plt
 import math
+import queue
+
+data_queue = queue.Queue();
 
 def callback(data, channel):
     if (data):
-        print("Received data of length {}".format(len(data)))
-        print("RMS of data is: {}".format(calc_rms(data)))
-        plt.plot(data)
-        plt.grid()
-        plt.show()
+        data_queue.put(data)
     else:
         print("Error receiving data on channel {} (Is scope in run mode?)".format(channel))
 
@@ -25,6 +24,14 @@ if __name__ == "__main__":
     b = PyOsziButtons
     scope = PyOszi(debug=False)
     scope.request_raw_data(0, callback)
-    #Scope communication is asynchronous, give it time to  finish
-    sleep(2)
+    try:
+        #wait up to 2 seconds for scope to return data
+        data = data_queue.get(timeout=2.0)
+        print("Received data of length {}".format(len(data)))
+        print("RMS of data is: {}".format(calc_rms(data)))
+        plt.plot(data)
+        plt.grid()
+        plt.show()
+    except queue.Empty as ex:
+        print("Timed out waitng for data from scope.")
     scope.close()
